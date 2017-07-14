3ba849084f91348204e7310befec1188a4090aba
System <none>
2017-07-14 01:41:17 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-var express  =require("express");
+//basic required import for NodeJs
+var express = require("express");
 var bodyParser = require("body-parser");
 var cors = require("cors");
 
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
+//get call to return JSON that format natural and unix data
+app.get('/dataValues/:dataVal', function(req, res, next){
+    //console.log('url works');
+    var dateVal = req.params.dateVal;
+    var dataFormattingOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    };
+    
+    if(isNaN(dateVal)){
+        var naturalDate = new Date(dateVal);
+        naturalDate = naturalDate.toLocaleDateString("en-us", dataFormattingOptions);
+        var unixDate = new Date(dateVal).getTime()/1000;
+    }
+    else{
+        var unixDate = dateVal;
+        var naturalDate = new Date(dateVal *1000);
+        naturalDate = naturalDate.toLocaleDateString("en-us", dataFormattingOptions);
+    }
+    res.json({unix: unixDate, natural: naturalDate});
+});
 
 app.listen(3000, function(){
-  console.log('working');
-})
+    console.log("It's working");
+});
