1d1931d642610e97ff00cfba5600d4ec7259f6d8
System <none>
2017-07-20 16:15:32 +0000

    Checkpoint

--- models/searchTerm.js
+++ models/searchTerm.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
+
+const searchTermSchema = new Schema(
+  {
+    searchVal: String,
+    searchDate: Date
+  },
+  {timestamp: true}
+);
+
+//connect model and collection
+const ModelClass = mongoose.model('searchTerm', searchTermSchema);
+module.exports = ModelClass;
