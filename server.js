1c12518e199a92806a21c1f7f7931290e01dc73c
System <none>
2017-07-20 09:11:32 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -3,57 +3,41 @@
  * the verification process may break
  * ***************************************************/
 
-'use strict';
+//basic required import for NodeJs
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
 
-var fs = require('fs');
-var express = require('express');
-var app = express();
-
-if (!process.env.DISABLE_XORIGIN) {
-  app.use(function(req, res, next) {
-    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-    var origin = req.headers.origin || '*';
-    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+var app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors());
+//get call to return JSON that format natural and unix data
+app.get('/dateValues/:dateVal', function(req, res, next){
+    console.log('url works');
+    var dateVal = req.params.dateVal;
+    var dataFormattingOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    };
+    
+    if(isNaN(dateVal)){
+        var naturalDate = new Date(dateVal);
+        naturalDate = naturalDate.toLocaleDateString("en-us", dataFormattingOptions);
+        var unixDate = new Date(dateVal).getTime()/1000;
     }
-    next();
-  });
-}
-
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.route('/_api/package.json')
-  .get(function(req, res, next) {
-    console.log('requested');
-    fs.readFile(__dirname + '/package.json', function(err, data) {
-      if(err) return next(err);
-      res.type('txt').send(data.toString());
-    });
-  });
-  
-app.route('/')
-    .get(function(req, res) {
-		  res.sendFile(process.cwd() + '/views/index.html');
-    })
-
-// Respond not found to all the wrong routes
-app.use(function(req, res, next){
-  res.status(404);
-  res.type('txt').send('Not found');
+    else{
+        var unixDate = dateVal;
+        var naturalDate = new Date(dateVal *1000);
+        naturalDate = naturalDate.toLocaleDateString("en-us", dataFormattingOptions);
+    }
+    res.json({unix: unixDate, natural: naturalDate});
 });
 
-// Error Middleware
-app.use(function(err, req, res, next) {
-  if(err) {
-    res.status(err.status || 500)
-      .type('txt')
-      .send(err.message || 'SERVER ERROR');
-  }  
-})
-
-app.listen(process.env.PORT, function () {
-  console.log('Node.js listening ...');
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
 });
 
+app.listen(3000, function(){
+    console.log("It's  working");
+});
